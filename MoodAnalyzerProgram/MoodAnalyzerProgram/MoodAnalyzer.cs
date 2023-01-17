a691c5447ae2661a78a5f1433956beeff956c6bd
qismat <qismatnisha6243@gmail.com>
2023-01-17 19:53:15 +0530

    [qismat] Add : UC1-AbilityToAnalyseHappyOrSad

---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/MoodAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        /// <summary>
+        /// Initializes a new instances of the <see cref="= "MoodAnalyser"/>class.
+        /// </summary>
+        public MoodAnalyzer()
+        {
+        }
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodAnalyzerProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How is your mood Happy or Sad");
+            string message = Console.ReadLine();
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            Console.WriteLine(moodAnalyzer.AnalyseMood(message));
+        }
+    }
+}
